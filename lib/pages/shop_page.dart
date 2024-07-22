8cd1250907f1932d487bb53ecd994caaa7c18796
SuperiorSam22 <sandeep221102@gmail.com>
2024-07-22 13:16:52 +0530

    added search ui and hot picks

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,6 +10,67 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('ShopPage'),);
+    return Column(
+      //search bar
+       children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search'),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+          ],
+          
+          ),
+        ),
+        //message 
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 25.0),
+          child: Text('Everyone flies... Some fly longer than others!'),
+        ),
+
+
+        //hot picks 
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Hot Picks!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+              ),
+              Text('see all',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 10,),
+
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ShoeTile();
+            },),),
+       ],
+
+    );
   }
 }
