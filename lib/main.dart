cb62f4284d1a18706673d3f3b20bf5af7b4cefe8
SuperiorSam22 <sandeep221102@gmail.com>
2024-07-23 11:23:58 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:e_commerce_application/pages/intro_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'models/cart.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
