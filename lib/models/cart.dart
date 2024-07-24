aa05a73dba71b89896539de1791998e72e554012
SuperiorSam22 <sandeep221102@gmail.com>
2024-07-24 15:20:41 +0530

    add remove and delete added in cart

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -9,28 +9,32 @@ class Cart extends ChangeNotifier{
       name: 'Zoom Freak', 
       price: '22,000',
       imagePath: 'lib/images/pngwing.com (4).png',
-      description: 'The forward thinking design of this latest signature shoe!'
+      description: 'The forward thinking design of this latest signature shoe!',
+      quantity: 1,
     ),
 
     Shoe(
       name: 'Air Jordans', 
       price: '80,000',
       imagePath: 'lib/images/pngwing.com (5).png',
-      description: 'The forward thinking design of this latest signature shoe!'
+      description: 'The forward thinking design of this latest signature shoe!',
+      quantity: 1,
     ),
 
     Shoe(
       name: 'Travis Scott AJ', 
       price: '2,25,000',
       imagePath: 'lib/images/pngwing.com (6).png',
-      description: 'The forward thinking design of this latest signature shoe!'
+      description: 'The forward thinking design of this latest signature shoe!',
+      quantity: 1,
     ),
 
     Shoe(
       name: 'KD Treys', 
       price: '29,000',
       imagePath: 'lib/images/pngwing.com (7).png',
-      description: 'The forward thinking design of this latest signature shoe!'
+      description: 'The forward thinking design of this latest signature shoe!',
+      quantity: 1,
     ),
   ];
 
@@ -49,13 +53,32 @@ class Cart extends ChangeNotifier{
 
   // add item to cart 
   void addItemToCart(Shoe shoe) {
+   int index = userCart.indexWhere((item) => item.name == shoe.name);
+   if(index >= 0){
+    userCart[index].quantity += 1;
+   } else {
     userCart.add(shoe);
-    notifyListeners();
+   }
+   notifyListeners();
   }
 
   //remove items from the cart 
   void removeItemFromCart(Shoe shoe) {
+    int index = userCart.indexWhere((item) => item.name == shoe.name);
+    if(index >= 0 && userCart[index].quantity > 1) {
+      userCart[index].quantity -= 1;
+    } else {
+      userCart.removeWhere((item) => item.name == shoe.name);
+    }
+    notifyListeners();
+  }
+
+  //delete items from the cart 
+  void deleteItemFromCart(Shoe shoe) {
+    int index = userCart.indexWhere((item) => item.name == shoe.name);
+    userCart[index].quantity = 1;
     userCart.remove(shoe);
     notifyListeners();
   }
+
 }
